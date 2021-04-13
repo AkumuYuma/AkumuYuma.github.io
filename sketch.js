e05937fd5032b72cf5a2d3eb671885c25bc5af24
AkumuYuma <manumanu1355@gmail.com>
2021-04-13 18:48:45 +0200

    Aggiunte librerie p5js

--- sketch.js
+++ sketch.js
@@ -1,14 +1,7 @@
-let v;
-
 function setup() {
-  createCanvas(400, 400); 
-  background(0);
-  v = p5.Vector.random2D; 
-
-} 
+  // put setup code here
+}
 
 function draw() {
-  stroke(255, 50);
-  translate(width/2, height/2);
-  line(0, 0, v.x, v.y);
+  // put drawing code here
 }
